c61ca3e8e222cd32160ff78a0596510611cd816c
= <=>
2024-10-06 22:42:16 +0200

    1-New version

--- letter-frequency.sh
+++ letter-frequency.sh
@@ -1,16 +1,30 @@
 #!/bin/bash
 #Author SHAOKAI CHEN
 
-filename="LittleWomen.txt" #Leer el archivo, usarlo como un variable $filename
-exportfilename="LittleWomen.lyr" 
+filename="LittleWomen.txt" # Leer el archivo, usarlo como una variable $filename
+temp="temp.txt" # Donde se guarda los resultados
 
-{
-tr -d '[:punct:][:digit:][:space:]' < $filename | #Eliminar punctuaiones, espacioes y numeros (-d: delete)
-tr '[:upper:]' '[:lower:]' | #Pasar todas las letras a minuscula
-fold -w1 | #Ajustar el texto de manera que ocupe 1 caracter cada linea (-w1: anchura 1)
-sort | #Reordenar las letras para que luego pueda usar (uniq -c) para contar
-uniq -c | #Se utiliza para eliminar letras repetidas consecutivas, pero con (-c) puede contar la cantidad
-#que aparecen las letras
-sort -rn; # (-n) Reordenar segun numeros, (-r) reverse
+echo "" > "temp.txt" # Crear un archivo vacio
+for letra in {a..z}; do # Para cada letra a,b,c,d...z
+	frecuencia=$(cat $filename | # Leer el archivo LittleWomen
+		tr '[:upper:]' '[:lower:]' | # Convertir todas las letras en minuscula
+		fold -w1 | # Ajustar el texto de manera que ocupe 1 caracter cada linea (-w1: anchura 1)
+		grep -o "$letra" | # Encontrar las lineas que contienen $letra
+		wc -l) # Contar
+	echo "$frecuencia $letra" >> "$temp" # Guardar los resultados en un archivo
+done
+sort -rn "$temp" > "LittleWomen.lyr" # Reordenar segun numeros, y guardar los resultados en LittleWomen.lyr
+rm "temp.txt"
 
-} > "$exportfilename" #Generar un archivo para guardar los datos
+# Otra forma (Mas rapida)------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+#{
+#tr -d '[:punct:][:space:][:digit:]' < $filename | #Eliminar punctuaiones, espacioes y numeros (-d: delete)
+#tr '[:upper:]' '[:lower:]' |
+#fold -w1 |
+#sort |
+#uniq -c | #Se utiliza para eliminar letras repetidas consecutivas, pero con (-c) puede contar la cantidad que aparece cada letra
+#sort -rn;
+#} > "$exportfilename"
+
+
+

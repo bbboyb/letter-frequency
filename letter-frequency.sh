b8bfddc9ace0b33774cfc0f7af46184ce69820b8
= <=>
2024-10-06 20:25:14 +0200

    1-Added notes

--- letter-frequency.sh
+++ letter-frequency.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 #Author SHAOKAI CHEN
 
-filename="LittleWomen.txt"
-tr -d '[:punct:][:digit:][:space:]' < $filename |
-tr '[:upper:]' '[:lower:]' |
-fold -w1 |
-sort |
-uniq -c |
-sort -rn
+filename="LittleWomen.txt" #Leer el archivo, usarlo como un variable $filename
+tr -d '[:punct:][:digit:][:space:]' < $filename | #Eliminar punctuaiones, espacioes y numeros (-d: delete)
+tr '[:upper:]' '[:lower:]' | #Pasar todas las letras a minuscula
+fold -w1 | #Ajustar el texto de manera que ocupe 1 caracter cada linea (-w1: anchura 1)
+sort | #Reordenar las letras para que luego pueda usar (uniq -c) para contar
+uniq -c | #Se utiliza para eliminar letras repetidas consecutivas, pero con (-c) puede contar la cantidad
+#que aparecen las letras
+sort -rn # (-n) Reordenar segun numeros, (-r) reverse

e6a91b054966febf4003d07997d75b1d0ae58697
= <=>
2024-10-06 20:48:11 +0200

    1-Generate a .lyr file instead of showing everything directly in the terminal

--- letter-frequency.sh
+++ letter-frequency.sh
@@ -2,10 +2,15 @@
 #Author SHAOKAI CHEN
 
 filename="LittleWomen.txt" #Leer el archivo, usarlo como un variable $filename
+exportfilename="LittleWomen.lyr" 
+
+{
 tr -d '[:punct:][:digit:][:space:]' < $filename | #Eliminar punctuaiones, espacioes y numeros (-d: delete)
 tr '[:upper:]' '[:lower:]' | #Pasar todas las letras a minuscula
 fold -w1 | #Ajustar el texto de manera que ocupe 1 caracter cada linea (-w1: anchura 1)
 sort | #Reordenar las letras para que luego pueda usar (uniq -c) para contar
 uniq -c | #Se utiliza para eliminar letras repetidas consecutivas, pero con (-c) puede contar la cantidad
 #que aparecen las letras
-sort -rn # (-n) Reordenar segun numeros, (-r) reverse
+sort -rn; # (-n) Reordenar segun numeros, (-r) reverse
+
+} > "$exportfilename" #Generar un archivo para guardar los datos
